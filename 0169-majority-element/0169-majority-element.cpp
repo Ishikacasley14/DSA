10346f62b0639addf6c6cdbe697659b052b9bae8
Ishikacasley14 <117714346+Ishikacasley14@users.noreply.github.com>
2025-09-05 20:49:12 +0530

    Time: 0 ms (100%), Space: 28 MB (86.73%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,21 +4,22 @@ public:
         int n = nums.size();
         int element = nums[0];
         int count = 1;
-        for(int i=1;i<n;i++){
-            if(count==0){
+        for (int i = 1; i < n; i++) {
+            if (count == 0) {
                 element = nums[i];
-                count=1;
-            }else if(element == nums[i]){
+                count = 1;
+            } else if (nums[i] == element) {
                 count++;
-            }else{
+            } else {
                 count--;
             }
         }
+        return element;
         int freq = 0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==element) freq++;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == element)
+                freq++;
         }
-        if(freq>n/2) return element;
-        return -1;
+        return freq / 2;
     }
 };
